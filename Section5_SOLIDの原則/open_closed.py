1ed1cb38c9660575000f783354aa4c48cb155cad
akitoshi.shiga <akitoshi.shiga@insight-inc.biz>
2021-09-24 11:04:00 +0900

    開放閉鎖の原則の復習

--- Section5_SOLIDの原則/open_closed.py
+++ Section5_SOLIDの原則/open_closed.py
@@ -7,24 +7,63 @@
   * 機能追加した時は、その部分だけテストすれば良くなる（デグレの試験など不要）
 """
 
-from abc import ABCMeta, abstractmethod
-
 class UserInfo:
 
-    def __init__(self, user_name, job_name, nationality):
+    def __init__(self, user_name: str, job_name: str, nationality: str):
         self.user_name   = user_name
         self.job_name    = job_name
         self.nationality = nationality
 
     def __str__(self):
         return '{}, {}, {}'.format(
-            self.user_name, self.job_name, self.nationality
-        )
+                    self.user_name, self.job_name, self.nationality
+                )
 
+
+# ダメな例
+"""
+* ダメな点
+* 新しいメソッドを加える時にUserInfoFilterをいじらなければいけない
+* UserInfoFilterに条件を判断する機能が組み込まれている
+"""
+class UserInfoFilter:
+
+    @staticmethod
+    def filter_users_job(users: list[UserInfo], job_name: str) -> UserInfo:
+        for user in users:
+            if user.job_name == job_name:
+                yield user
+
+    @staticmethod
+    def filter_users_nationality(users: list[UserInfo], nationality: str) -> UserInfo:
+        for user in users:
+            if user.nationality == nationality:
+                yield user
+
+
+# client
+taro = UserInfo('taro', 'salary man', 'Japan')
+jiro = UserInfo('jiro', 'police man', 'Japan')
+john = UserInfo('john', 'salary man', 'USA')
+user_list = [taro, jiro, john]
+
+for man in UserInfoFilter.filter_users_job(user_list, 'police man'):
+    print(man)
+
+for man in UserInfoFilter.filter_users_nationality(user_list, 'Japan'):
+    print(man)
+
+
+# 良い例
+# 抽象クラスを使える用にする
+from abc import ABCMeta, abstractmethod
+
+# UserInfoFilterの機能を分割する
+# UserInfoFilterをの条件を判断する機能を抽象クラスとして定義する
 class Comparation(metaclass=ABCMeta):
 
     @abstractmethod
-    def is_equal(self, other):
+    def is_equal(self, other: UserInfo) -> bool:
         pass
 
     def __and__(self, other):
@@ -33,13 +72,30 @@ class Comparation(metaclass=ABCMeta):
     def __or__(self, other):
         return OrComparation(self, other)
 
+class JobNameComparation(Comparation):
+
+    def __init__(self, job_name: str):
+        self.job_name = job_name
+
+    def is_equal(self, other: UserInfo) -> bool:
+        return  self.job_name == other.job_name
+
+
+class NationalityComparation(Comparation):
+
+    def __init__(self, nationality: str):
+        self.nationality = nationality
+
+    def is_equal(self, other: UserInfo) -> bool:
+        return self.nationality == other.nationality
+
 
 class AndComparation(Comparation):
 
-    def __init__(self, *args):
+    def __init__(self, *args: Comparation):
         self.comparations = args
 
-    def is_equal(self, other):
+    def is_equal(self, other: list[UserInfo]) -> UserInfo:
         return all(
             map(
                 lambda comparation: comparation.is_equal(other),
@@ -47,100 +103,56 @@ class AndComparation(Comparation):
             )
         )
 
-
 class OrComparation(Comparation):
 
-    def __init__(self, *args):
+    def __init__(self, *args: Comparation):
         self.comparations = args
 
-    def is_equal(self, other):
+    def is_equal(self, other: list[UserInfo]) -> UserInfo:
         return any(
             map(
+                # フィールドのis_equalが実行される
                 lambda comparation: comparation.is_equal(other),
                 self.comparations
             )
         )
-
-
-class JobNameComparation(Comparation):
-
-    def __init__(self, job_name):
-        self.job_name = job_name
-
-    def is_equal(self, other):
-        return self.job_name == other.job_name
-
-
-class NationalityComparation(Comparation):
-
-    def __init__(self, nationality):
-        self.nationality = nationality
-
-    def is_equal(self, other):
-        return self.nationality == other.nationality
-
-
-
+# UserInfoFilterをの条件にあったuserを返す機能を抽象クラスとして定義する
 class Filter(metaclass=ABCMeta):
 
+    # ここしれっと多態性になってる
     @abstractmethod
-    def filter(self, comparation, item):
+    def filter(self, comparation: Comparation, items: list[UserInfo]) -> UserInfo:
         pass
 
-class UserInfoFilter(Filter):
 
-    def filter(self, comparation, items):
+class UserInfoFilter2(Filter):
+
+    def filter(self, comparation: Comparation, items: list[UserInfo]) -> UserInfo:
         for item in items:
-            if comparation.is_equal(item):
+            if comparation.is_equal(item): #ここの処理を移譲することで汎用的になる
                 yield item
 
-"""
-class UserInfoFilter:
-    現在の実装では機能を追加する時に元のメソッドをいじらなければならず
-    良くない
 
-    @staticmethod
-    def filter_users_job(users, job_name):
-        for user in users:
-            if user.job_name == job_name:
-                yield user
-
-    @staticmethod
-    def filter_users_nationality(users, nationality):
-        for user in users:
-            if user.nationality == nationality:
-                yield user
-"""
-    # 機能の変更がひつようとなった場合、新しいメソッドを作らなければならない
-
-taro = UserInfo('taro', 'salary man', 'Japan');
-jiro = UserInfo('jiro', 'police man', 'Japan');
-john = UserInfo('john', 'salary man', 'USA');
-
+# client
+taro = UserInfo('taro', 'salary man', 'Japan')
+jiro = UserInfo('jiro', 'police man', 'Japan')
+john = UserInfo('john', 'salary man', 'USA')
 user_list = [taro, jiro, john]
 
 salary_man_comparation = JobNameComparation('salary man')
-japan_comparation = NationalityComparation('Japan')
-user_info_filter = UserInfoFilter()
-
-
+user_info_filter = UserInfoFilter2()
 for user in user_info_filter.filter(salary_man_comparation, user_list):
     print(user)
+
+japan_comparation = NationalityComparation('Japan')
 for user in user_info_filter.filter(japan_comparation, user_list):
     print(user)
 
-
-print('-'*100)
+# Comparationを&で挟むとオーバーライドされたandが実行される
+# つまり、ここで宣言された変数はAndComparationオブジェクトが返される
 salary_man_and_japan = salary_man_comparation & japan_comparation
 for user in user_info_filter.filter(salary_man_and_japan, user_list):
     print(user)
-
-print('-'*100)
 salary_man_or_japan = salary_man_comparation | japan_comparation
 for user in user_info_filter.filter(salary_man_or_japan, user_list):
     print(user)
-"""
-for man in UserInfoFilter.filter_users_nationality(user_list, 'Japan'):
-    print(man)
-"""
-
